780e53cd14be8df3271a15ad018bd37606bbcb81
henryust12 <henryust12@gmail.com>
2022-02-13 06:00:44 +0900

    Java: SpringBoot Init

---
+++ Java/ElonMuskBookStore/src/main/java/eden/elonmuskbookstore/ElonMuskBookStoreApplication.java
@@ -0,0 +1,13 @@
+package eden.elonmuskbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElonMuskBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElonMuskBookStoreApplication.class, args);
+    }
+
+}
---
+++ Java/ElonMuskBookStore/src/test/java/eden/elonmuskbookstore/ElonMuskBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package eden.elonmuskbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElonMuskBookStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
