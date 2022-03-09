3ba3573a969d76c709ddbaa21871d57622ee1dfc
henryust12 <henryust12@gmail.com>
2022-03-09 14:11:28 +0900

    finished テーブル設計書

--- Java/ElonBookStore-java8/src/main/java/com/eden/ElonBookStore/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.eden.ElonBookStore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.eden.ElonBookStore.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-
-}
